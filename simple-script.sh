fc0aefcd2272b0a320c0e110683f06f08a52b9f5
Sol Varietti <mv288481@fi365.ort.edu.uy>
2025-02-17 22:16:02 -0300

    Creamos el inventario con variables

---
+++ simple-script.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Uso de disco"
+df -h
+echo "===="
+echo "Uptime del servidor"
+uptime -p 
+echo "===="
+if [ -e /etc/debian_version ]; then 
+	exit
+fi
+echo "Estado de SELinux"
+sestatus |grep '^SELinux status'
+echo "=FIN="
+
